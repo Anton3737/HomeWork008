4f805c49ff5bbceaec6ae8220b8e90b69c0dc39a
Barash Anton <BarashASUA@gmail.com>
2022-06-19 23:24:13 +0300

    Calculator done

--- src/Delivery.java
+++ src/Delivery.java
@@ -5,15 +5,24 @@ public class Delivery {
         System.out.println("Введіть кількість клєнтів: ");
         Scanner in = new Scanner(System.in);
         System.out.println("Факторіал з числа "+ in.nextInt() + " становить " + factorial(in.nextInt()));
+        System.out.println("Факторіал з числа + Recurtion " + in.nextInt() + " становить " + factorialRecurtion(in.nextInt()));
     }
     public static int factorial(int N){
         int factor  = 1;
         int i = 1;
         do {
-           factor = factor * i;
-           ++i;
+           factor = factor * i;  // через цикл do - while
+           i++;
         }
         while (i <= N);
             return factor;
+
+    }
+
+    public static int factorialRecurtion(int n) {
+        if (n <= 1)
+            return 1;        // через Рекурсию
+        else
+            return n * factorialRecurtion(n-1);
     }
 }
